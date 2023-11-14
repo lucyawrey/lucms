15dc0796051995ea7bf28b2b8a9d4672301d20f7
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-11-14 15:07:55 -0500

    wip move to postgres and create new initial migration for CMS tables

--- migrations/20231101074350_initial.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-CREATE TABLE todos (
-    id          INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
-    description TEXT    NOT NULL,
-    done        BOOLEAN NOT NULL DEFAULT FALSE
-);
